88f02d988456019b56e04047044fc86a3ac333ac
Phil <knamustarsunder@gmail.com>
2020-02-26 18:46:54 -0800

    added second function and test unit test

--- task.py
+++ task.py
@@ -7,3 +7,12 @@ def firstrun():
 
 def radiusToArea(radius):
     return math.pi * math.pow(radius, 2)
+
+
+def firstLast(listIn):
+    if len(listIn) <= 0:
+        return listIn
+    else:
+        newList = listIn.copy()
+        del newList[1:len(newList)-1]
+        return newList
--- test_task.py
+++ test_task.py
@@ -12,3 +12,26 @@ class Test(TestCase):
             testResult = task.radiusToArea(testInput)  # call function
             self.assertEqual(testResult, math.pi * math.pow(testInput, 2))  # assert accurate performance of function
         pass
+
+
+    def test_first_last(self):
+        random.seed()
+
+        # test length of 0
+        testList = []
+        self.assertListEqual(task.firstLast(testList), testList)
+
+        # test length of 1
+        testList = ['a']
+        self.assertListEqual(task.firstLast(testList), testList)
+
+        # test random length greater than 1
+        testList = []
+        for x in range(1000):
+            listLen = random.randint(2, 1000)
+            for x in range(listLen):
+                testList.append(random.randint(0, 1000))
+            listCopy = testList.copy()
+            del listCopy[1:len(testList) - 1]
+            self.assertListEqual(task.firstLast(testList), listCopy)
+        pass
