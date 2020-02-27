e752701597831d4a3ec448f4fbeab0f3ea71b687
Phil <knamustarsunder@gmail.com>
2020-02-26 18:06:00 -0800

    reformatted code slightly

--- task.py
+++ task.py
@@ -1,10 +1,10 @@
 import math
 import string
 
+
 def firstrun():
     return "success"
 
 
 def radiusToArea(radius):
     return math.pi * math.pow(radius, 2)
-
--- test_task.py
+++ test_task.py
@@ -8,7 +8,7 @@ class Test(TestCase):
     def test_radius_to_area(self):
         random.seed()
         for x in range(1000):
-            testInput = random.randint(0, 1000) + random.random() # set up variables
-            testResult = task.radiusToArea(testInput) # call function
-            self.assertEqual(testResult, math.pi * math.pow(testInput, 2)) # assert accurate performance of function
+            testInput = random.randint(0, 1000) + random.random()  # set up variables
+            testResult = task.radiusToArea(testInput)  # call function
+            self.assertEqual(testResult, math.pi * math.pow(testInput, 2))  # assert accurate performance of function
         pass
